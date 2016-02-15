3e4a85c7840d49942fd3d77dcbc46cee25f9fc9b
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-02-15 20:51:40 +0300

    Added UpdatePhoneOfferCount job. Added phone_count column to phones table.

---
+++ app/Jobs/UpdatePhoneOfferCount.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Jobs\Job;
+use App\Models\Phone;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UpdatePhoneOfferCount extends Job implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    private $phone;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Phone $phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->phone->save(
+            [
+                'offer_count' => $this->phone->offers()->count()
+            ]
+        );
+    }
+}
---
+++ database/migrations/2016_02_15_203511_add_offer_count_to_phones_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOfferCountToPhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('phones', function (Blueprint $table) {
+            $table->integer('offer_count')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('phones', function (Blueprint $table) {
+            $table->dropColumn('offer_count');
+        });
+    }
+}
