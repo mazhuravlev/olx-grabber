1883455c3d914e065be00a1c81bdcbb3b408c5c2
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-02-20 14:48:52 +0300

    Display progressbar in details:parameters:parse command.

--- app/Console/Commands/ParseDetailsParameters.php
+++ app/Console/Commands/ParseDetailsParameters.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\DetailsParameter;
 use App\Models\Offer;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\QueryException;
 
 class ParseDetailsParameters extends Command
@@ -14,7 +15,7 @@ class ParseDetailsParameters extends Command
      *
      * @var string
      */
-    protected $signature = 'details:parameters:parse';
+    protected $signature = 'details:parameters:parse {--info}';
 
     /**
      * The console command description.
@@ -23,6 +24,9 @@ class ParseDetailsParameters extends Command
      */
     protected $description = 'Parse details parameters from offers';
 
+
+    private $enableInfo;
+
     /**
      * Execute the console command.
      *
@@ -30,23 +34,52 @@ class ParseDetailsParameters extends Command
      */
     public function handle()
     {
-        $offers = Offer::query()->get(['details']);
-        foreach ($offers as $offer) {
-            foreach ($offer->details as $key => $value) {
-                $detailsParameter = DetailsParameter::firstOrCreate(
-                    ['parameter' => $key]
-                );
-                try {
-                    $detailsValue = $detailsParameter->detailsValues()
-                        ->firstOrCreate(
-                            ['value' => $value]
-                        );
-                } catch (QueryException $e) {
-                    if (23000 !== intval($e->getCode())) {
-                        throw $e;
+        $this->enableInfo = $this->option('info');
+        $cache = [];
+        $bar = $this->enableInfo ? null : $bar = $this->output->createProgressBar(Offer::count());
+        $offers = Offer::query()->chunk(100, function (Collection $offers) use ($bar, &$cache) {
+            $this->info("Got {$offers->count()} offers");
+            foreach ($offers as $offer) {
+                if ($bar) {
+                    $bar->advance();
+                }
+                foreach ($offer->details as $key => $value) {
+                    if (array_key_exists($key, $cache)) {
+                        if (array_key_exists($value, $cache[$key])) {
+                            $this->info("$key:$value exists in cache");
+                            continue;
+                        }
+                    } else {
+                        $cache[$key] = [];
+                    }
+                    $detailsParameter = DetailsParameter::firstOrCreate(
+                        ['parameter' => $key]
+                    );
+                    try {
+                        $detailsValue = $detailsParameter->detailsValues()
+                            ->firstOrCreate(
+                                ['value' => $value]
+                            );
+                        if ($detailsValue->wasRecentlyCreated) {
+                            array_push($cache[$key], $value);
+                            $this->info("Added $key:$value");
+                        }
+                    } catch (QueryException $e) {
+                        if (23000 !== intval($e->getCode())) {
+                            throw $e;
+                        } else {
+                            $this->info("Skipped $key:$value");
+                        }
                     }
                 }
             }
+        });
+    }
+
+    public function info($string, $verbosity = null)
+    {
+        if ($this->enableInfo) {
+            parent::info($string, $verbosity);
         }
     }
 }
