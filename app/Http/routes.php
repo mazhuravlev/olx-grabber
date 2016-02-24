68415a5244ae89d363c8cb4fdf1715db9d92a201
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-02-24 17:10:33 +0300

    Added user auth.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,16 +3,17 @@
 Route::get('/', function () {
     return Redirect::to('/offers');
 });
-Route::get('/offers', 'OfferController@index');
-Route::get('/offer/olx_id/{olxId}', 'OfferController@findByOlxId');
-Route::get('/offer/{id}', 'OfferController@offer');
-Route::get('/phones', 'PhoneController@index');
-Route::get('/phone/{id}', 'PhoneController@phone');
-Route::get('/locations', 'LocationController@index');
+Route::get('/offers', ['middleware' => ['web', 'auth'], 'OfferController@index']);
+Route::get('/offer/olx_id/{olxId}', ['middleware' => ['web', 'auth'], 'OfferController@findByOlxId']);
+Route::get('/offer/{id}', ['middleware' => ['web', 'auth'], 'OfferController@offer']);
+Route::get('/phones', ['middleware' => ['web', 'auth'], 'PhoneController@index']);
+Route::get('/phone/{id}', ['middleware' => ['web', 'auth'], 'PhoneController@phone']);
+Route::get('/locations', ['middleware' => ['web', 'auth'], 'LocationController@index']);
 
 Route::group(
     [
-        'prefix' => 'api'
+        'prefix' => 'api',
+        'middleware' => ['web', 'auth']
     ],
     function () {
         Route::get('/offers/{timestamp?}', 'ApiController@offers');
@@ -23,7 +24,8 @@ Route::group(
 
 Route::group(
     [
-        'prefix' => 'details'
+        'prefix' => 'details',
+        'middleware' => ['web', 'auth']
     ],
     function () {
         Route::get('/', 'DetailsController@index');
@@ -33,7 +35,8 @@ Route::group(
 
 Route::group(
     [
-        'prefix' => 'export'
+        'prefix' => 'export',
+        'middleware' => ['web', 'auth']
     ],
     function () {
         Route::get('/', 'ExportController@index');
@@ -43,7 +46,8 @@ Route::group(
 
 Route::group(
     [
-        'prefix' => 'rest'
+        'prefix' => 'rest',
+        'middleware' => ['web', 'auth']
     ], function () {
     Route::resource('location', 'LocationController');
     Route::resource('details_parameter', 'DetailsParameterController');
