10eb42cef6ce20bfc09858af3f3f5981cd79f448
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-02-24 17:00:53 +0300

    Added Region model, migration and seeder.

---
+++ app/Models/Region.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Region extends Model
+{
+
+    public $incrementing = false;
+    public $timestamps = false;
+    public $fillable = [
+        'id',
+    ];
+
+}
---
+++ database/seeds/RegionsSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\Region;
+use Illuminate\Database\Seeder;
+
+class RegionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $regions = [
+            'sev', 'simf', 'ubk', 'evp', 'feo'
+        ];
+
+        foreach ($regions as $region) {
+            Region::create(['id' => $region]);
+        }
+    }
+}
