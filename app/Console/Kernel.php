f1bc3c55f573635a5c410e249763cc3ceb5296fd
Mikhail Zhuravlev <m.zhuravlev61@gmail.com>
2016-02-25 11:52:23 +0300

    Extracted scheduled task 'grab_links' to class GrabLinks

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,20 +3,11 @@
 namespace App\Console;
 
 use App\Console\Commands\ParseDetailsParameters;
-use App\Jobs\Parse;
-use App\Models\GrabbedUrl;
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ServerException;
 use Illuminate\Console\Scheduling\Schedule;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
-use Log;
-use Symfony\Component\DomCrawler\Crawler;
 
 class Kernel extends ConsoleKernel
 {
-
-    use DispatchesJobs;
 
     /**
      * The Artisan commands provided by your application.
@@ -35,54 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function () {
-            $client = new Client();
-            $baseUrl = 'http://olx.ua/nedvizhimost/cri/?page=%d';
-            $page = 1;
-            do {
-                $time = time();
-                $response = null;
-                try {
-                    $response = $client->get(sprintf($baseUrl, $page++));
-                } catch (ServerException $e) {
-                    Log::error('OLX server error', ['code' => $e->getCode(), 'message' => $e->getMessage()]);
-                    return;
-                }
-                $urls = [];
-                if ($response) {
-                    $crawler = new Crawler($response->getBody()->getContents());
-                    $urls = array_map(
-                        function ($link) {
-                            return preg_replace('/#.*$/', '', $link);
-                        },
-                        $crawler->filter('td.offer a.link')->extract('href')
-                    );
-                }
-                if (0 === count($urls)) {
-                    Log::info('Grabbed 0 OLX links');
-                }
-                $grabbedCount = 0;
-                $droppedCount = 0;
-                foreach ($urls as $url) {
-                    $grabbedUrl = GrabbedUrl::where('url', $url)->first();
-                    if ($grabbedUrl) {
-                        /** @var GrabbedUrl $grabbedUrl */
-                        $grabbedUrl->count = $grabbedUrl->count + 1;
-                        $grabbedUrl->save();
-                        $droppedCount++;
-                    } else {
-                        $grabbedUrl = GrabbedUrl::create(
-                            [
-                                'url' => $url
-                            ]
-                        );
-                        $this->dispatch(new Parse($grabbedUrl));
-                        $grabbedCount++;
-                    }
-                }
-                printf('[%d] grabbed: %d, dropped: %d' . PHP_EOL, time() - $time, $grabbedCount, $droppedCount);
-            } while ($grabbedCount > 0);
-        })
+        $schedule->call('App\System\Schedule\GrabLinks::grabLinks')
             ->name('grab_links')
             ->withoutOverlapping()
             ->everyMinute();
---
+++ app/System/Schedule/GrabLinks.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\System\Schedule;
+
+
+use App\Jobs\Parse;
+use App\Models\GrabbedUrl;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ServerException;
+use Illuminate\Bus\Dispatcher;
+use Log;
+use Symfony\Component\DomCrawler\Crawler;
+
+class GrabLinks
+{
+
+    public static function grabLinks()
+    {
+        $client = new Client();
+        $baseUrl = 'http://olx.ua/nedvizhimost/cri/?page=%d';
+        $page = 1;
+        do {
+            $time = time();
+            $response = null;
+            try {
+                $response = $client->get(sprintf($baseUrl, $page++));
+            } catch (ServerException $e) {
+                Log::error('OLX server error', ['code' => $e->getCode(), 'message' => $e->getMessage()]);
+                return;
+            }
+            $urls = [];
+            if ($response) {
+                $crawler = new Crawler($response->getBody()->getContents());
+                $urls = array_map(
+                    function ($link) {
+                        return preg_replace('/#.*$/', '', $link);
+                    },
+                    $crawler->filter('td.offer a.link')->extract('href')
+                );
+            }
+            if (0 === count($urls)) {
+                Log::info('Grabbed 0 OLX links');
+            }
+            $grabbedCount = 0;
+            $droppedCount = 0;
+            /** @var Dispatcher $dispatcher */
+            $dispatcher = app('Illuminate\Bus\Dispatcher');
+            foreach ($urls as $url) {
+                $grabbedUrl = GrabbedUrl::where('url', $url)->first();
+                if ($grabbedUrl) {
+                    /** @var GrabbedUrl $grabbedUrl */
+                    $grabbedUrl->count = $grabbedUrl->count + 1;
+                    $grabbedUrl->save();
+                    $droppedCount++;
+                } else {
+                    $grabbedUrl = GrabbedUrl::create(
+                        [
+                            'url' => $url
+                        ]
+                    );
+                    $dispatcher->dispatch(new Parse($grabbedUrl));
+                    $grabbedCount++;
+                }
+            }
+            printf('[%d] grabbed: %d, dropped: %d' . PHP_EOL, time() - $time, $grabbedCount, $droppedCount);
+        } while ($grabbedCount > 0);
+    }
+
+}
